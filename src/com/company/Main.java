168123a55873d08f6eef0c980ba516833a43a898
Anastasia <chaika.a@phystech.edu>
2022-03-03 17:11:10 +0300

    Third commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First commit in develop branch");
         System.out.println("Second commit in develop branch");
+        System.out.println("Third commit in develop branch");
     }
 }
