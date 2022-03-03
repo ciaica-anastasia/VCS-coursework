13a766e43a73b8e1b2046f5e2aac38a243d91d05
Anastasia <chaika.a@phystech.edu>
2022-03-03 17:30:56 +0300

    Second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Second commit in develop branch");
         System.out.println("Third commit in develop branch");
         System.out.println("Adding major feature for next release");
+        System.out.println("Second commit in major_feature branch");
     }
 }
